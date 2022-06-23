e3e81200145c9f1a4b8f121276f725a264a6eeba
Eli Bloom <rollinggoron@gmail.com>
2022-06-23 15:34:18 -0400

    added more features

--- src/App.jsx
+++ src/App.jsx
@@ -1,130 +1,172 @@
 import React, { useState, useEffect } from "react";
 import "./styles/App.scss";
 import DisplayGitInfo from "./components/info-loader/DisplayGitInfo";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import ClearIcon from '@mui/icons-material/Clear';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import ListSubheader from '@mui/material/ListSubheader';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import ClearIcon from "@mui/icons-material/Clear";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import ListSubheader from "@mui/material/ListSubheader";
+import Avatar from "@mui/material/Avatar";
+import Switch from "@mui/material/Switch";
+import Alert from "@mui/material/Alert";
 
-
-import SquareIcon from '@mui/icons-material/Square';
-
+import SquareIcon from "@mui/icons-material/Square";
 
 export default function App() {
   const baseUrl = "https://api.github.com/orgs/";
   const [primaryLoadError, setPrimaryLoadError] = useState("");
-  const [baseJSON, setBaseJson] = useState({});
+  const [baseJson, setBaseJson] = useState({});
+  const [isCreatedByNewer, setIsCreatedByNewer] = useState(false);
+  const [isUpdatedByNewer, setIsUpdatedByNewer] = useState(false);
+
   const [gitEndpoint, setGitEndpoint] = useState(
     "https://api.github.com/orgs/boomtownroi"
   );
+  const [isAccountVerified, setIsAccontVerified] = useState(false);
+  /**
+   *
+   * */
+  async function loadBaseJson() {
+    if (gitEndpoint.includes(baseUrl)) {
+      let response = await fetch(gitEndpoint);
 
-  async function loadGitInfo() {
-    let response = await fetch(gitEndpoint);
-
-    let json = await response.json();
-    if (!response.ok) {
-      console.log("errr");
+      let json = await response.json();
+      if (!response.ok) {
+        setPrimaryLoadError(response.status);
+      } else {
+        setBaseJson(json);
+        setIsAccontVerified(true);
+      }
     }
-
-    setBaseJson(json);
-
-    console.log(json.is_verified)
-
-  
-
-    console.log(json);
   }
 
   useEffect(() => {
-    loadGitInfo();
-  },[gitEndpoint]);
+    loadBaseJson();
+  });
+
+  /*********************************
+  * Event Handlers
+  /*********************************
 
   /**
-   * Event Handlers
-   */
+   * 
+   * */
+  function clearGitEndpoint() {
+    setGitEndpoint("");
+  }
+  function changeEndpoint(event) {
+    setGitEndpoint(event.target.value);
+  }
+  function closeErrorAlert() {
+    setPrimaryLoadError("");
+  }
 
+  return (
+    <div className="app">
+      <div className="endpoint-input-div">
+        <Avatar
+          id="git-repo-avatar"
+          alt="GIT repo avatar"
+          src={baseJson?.avatar_url}
+          sx={{ width: 100, height: 100 }}
+        />
 
-  function clearGitEndpoint(){}
-
-  return <div className="App">
-    <div className="endpoint-input-div">
-      <TextField id="endpoint-input" label="Enter Desired Endpoint" varient="filled" defaultValue={gitEndpoint}></TextField>
-      <Button id="clear-text-button" label="Clear" onClick={clearGitEndpoint} variant="outlined"><ClearIcon/></Button>
-    </div>
-    <div className="primary-fields-list">
-      {/* the primary fields */}
-      <List  subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Primary Fields on GIT API Endpoint
-        </ListSubheader>
-      }>
-          <ListItem disablePadding>
-              <ListItemIcon>
-                <SquareIcon fontSize="1px"/>
-              </ListItemIcon>
-                <ListItemText >
-                  Id: {baseJSON.id}
-                </ListItemText>
+        <TextField
+          id="endpoint-input"
+          label="Enter Desired Endpoint"
+          varient="filled"
+          defaultValue={gitEndpoint}
+          value={gitEndpoint}
+          onChange={changeEndpoint}
+        ></TextField>
+        <Button
+          id="clear-text-button"
+          label="Clear"
+          onClick={clearGitEndpoint}
+          variant="outlined"
+        >
+          <ClearIcon />
+        </Button>
+      </div>
+      {primaryLoadError && (
+        <Alert variant="filled" severity="error" onClose={closeErrorAlert}>
+          There has been an error loading from GIT API : {primaryLoadError}
+        </Alert>
+      )}
+      <div className="primary-fields-list">
+        {/* the primary fields */}
+        <List
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              Primary Fields on GIT API Endpoint
+            </ListSubheader>
+          }
+        >
+          <ListItem>
+            <ListItemIcon>
+              <SquareIcon fontSize="1px" />
+            </ListItemIcon>
+            <ListItemText>Id: {baseJson.id}</ListItemText>
           </ListItem>
           <Divider />
-          <ListItem disablePadding>
-              <ListItemIcon>
-                <SquareIcon  fontSize="1px"/>
-              </ListItemIcon>
-              <ListItemText >
-                Name: {baseJSON.name}
-              </ListItemText>
+          <ListItem>
+            <ListItemIcon>
+              <SquareIcon fontSize="1px" />
+            </ListItemIcon>
+            <ListItemText>Name: {baseJson.name}</ListItemText>
           </ListItem>
           <Divider />
-          <ListItem disablePadding>
-              <ListItemIcon>
-                <SquareIcon fontSize="1px"/>
-              </ListItemIcon>
-              <ListItemText >
-                Html_url: {baseJSON.html_url}
-              </ListItemText>
+          <ListItem>
+            <ListItemIcon>
+              <SquareIcon fontSize="1px" />
+            </ListItemIcon>
+            <ListItemText>Html_url: {baseJson.html_url}</ListItemText>
           </ListItem>
           <Divider />
-          <ListItem disablePadding>
-              <ListItemIcon>
-                <SquareIcon fontSize="1px"/>
-              </ListItemIcon>
-              <ListItemText >
-                is_verified: {baseJSON.is_verified}
-              </ListItemText>
+          <ListItem>
+            <ListItemIcon>
+              <SquareIcon fontSize="1px" />
+            </ListItemIcon>
+            <ListItemText>
+              Account Verified:{" "}
+              <Switch label="sdf" disabled={!isAccountVerified} />
+            </ListItemText>
+            {/* <ListItemText>Verified Account: {baseJson.is_verified}</ListItemText> */}
           </ListItem>
           <Divider />
-          <ListItem disablePadding>
-              <ListItemIcon>
-                <SquareIcon fontSize="1px"/>
-              </ListItemIcon>
-              <ListItemText >
-                Created_at: {baseJSON.created_at}
+          <ListItem>
+            <ListItemIcon>
+              <SquareIcon fontSize="1px" />
+            </ListItemIcon>
+            {isCreatedByNewer ? (
+              <ListItemText>
+                Created_at: {baseJson.created_at} **Newer Field**
               </ListItemText>
+            ) : (
+              <ListItemText>Created_at: {baseJson.created_at} </ListItemText>
+            )}
           </ListItem>
           <Divider />
-          <ListItem disablePadding>
-              <ListItemIcon>
-                <SquareIcon fontSize="1px"/>
-              </ListItemIcon>
-              <ListItemText >
-                updated_at: {baseJSON.updated_at}
+          <ListItem>
+            <ListItemIcon>
+              <SquareIcon fontSize="1px" />
+            </ListItemIcon>
+            {isUpdatedByNewer ? (
+              <ListItemText>
+                updated_at: {baseJson.updated_at} **Newer Field**
               </ListItemText>
+            ) : (
+              <ListItemText>updated_at: {baseJson.updated_at}</ListItemText>
+            )}
           </ListItem>
         </List>
+      </div>
+      {/* <DisplayGitInfo className="display-git-info" json={baseJson} /> */}
     </div>
-    <List  subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Primary Fields on GIT API Endpoint
-        </ListSubheader>
-      }>
-      <DisplayGitInfo/>
-    </List>
-    </div>;
+  );
 }
--- src/components/info-loader/DisplayGitInfo.jsx
+++ src/components/info-loader/DisplayGitInfo.jsx
@@ -1,55 +1,127 @@
 import React, { useState, useEffect } from "react";
 import "../../styles/App.scss";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import ClearIcon from '@mui/icons-material/Clear';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import ListSubheader from '@mui/material/ListSubheader';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import ClearIcon from "@mui/icons-material/Clear";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import ListSubheader from "@mui/material/ListSubheader";
+import RelevantFields from "./json/GitApiFields.json";
+import SquareIcon from "@mui/icons-material/Square";
+import Collapse from "@mui/material/Collapse";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-
-import SquareIcon from '@mui/icons-material/Square';
-
-
-export default function DisplayGitInfo({baseJson}) {
+export default function DisplayGitInfo({ json }) {
   const baseUrl = "https://api.github.com/orgs/";
+  const [topLevelUrls, setTopLevelUrls] = useState();
   const [primaryLoadError, setPrimaryLoadError] = useState("");
-  const [baseJSON, setBaseJson] = useState({});
+  // const [baseJSON, setBaseJson] = useState({});
+  const [lists, setLists] = useState();
   const [gitEndpoint, setGitEndpoint] = useState(
     "https://api.github.com/orgs/boomtownroi"
   );
 
-  async function loadGitInfo() {
-    let response = await fetch(gitEndpoint);
+  /**
+   * gets the Urls that are on the orginizational level
+   * */
+  function getTopLevelUrls() {
+    let topLevelUrlsArr = Object.keys(RelevantFields);
+    return topLevelUrlsArr;
+  }
 
-    let json = await response.json();
-    if (!response.ok) {
+  /**
+   * creates the nested lists for the required fields found from wetching the top level usrl's
+   * */
+  function createFieldsLists() {
+    let topLevelUrls = getTopLevelUrls();
+    let elements = (
+      <List
+        className="fields"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            Top Level URL's
+          </ListSubheader>
+        }
+      >
+        {topLevelUrls.map((url) => {
+          if (Object.keys(json).length !== 0) {
+            let newUrl = json[url];
+            let info = loadGitInfo(newUrl);
+
+            return (
+              <>
+                <ListItem key={info?.id}>
+                  <ListItemIcon>
+                    <SquareIcon fontSize="1px" />
+                  </ListItemIcon>
+                  <ListItemText>{url}</ListItemText>
+                  <List>
+                    {RelevantFields[url].map((item) => {
+                      console.log(item);
+                      if (typeof item === "string" || item instanceof String) {
+                        return (
+                          <>
+                            <ListItem>
+                              <ListItemIcon>
+                                <SquareIcon fontSize="1px" />
+                              </ListItemIcon>
+                              <ListItemText>
+                                {item}: {info[item]}
+                              </ListItemText>
+                            </ListItem>
+                          </>
+                        );
+                      }
+                    })}
+                  </List>
+                </ListItem>
+                <Divider />
+              </>
+            );
+          }
+        })}
+      </List>
+    );
+    return elements;
+  }
+
+  /**
+   * helps unpack when fields that are loaded are nested
+   * */
+  function recursiveHelper() {}
+
+  async function loadGitInfo(url) {
+    if (url) {
+      let response = await fetch(url);
+
+      let json = await response.json();
+      if (!response.ok) {
         console.log("errr");
+      } else {
+        return json;
       }
-    setBaseJson(json);
 
-
-    
-
-    console.log(json);
+      console.log(json);
+    }
   }
 
   useEffect(() => {
-    loadGitInfo();
-  },[gitEndpoint]);
+    createFieldsLists();
+  }, [json]);
 
   /**
    * Event Handlers
    */
 
+  function clearGitEndpoint() {}
 
-  function clearGitEndpoint(){}
-
-  return <div className="App">
-   
-    </div>;
+  return <div>{createFieldsLists()}</div>;
 }
